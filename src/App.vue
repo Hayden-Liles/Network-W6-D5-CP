d5d3e10e50f0da575230d73be00d2cac2b367817
Hayden Liles <lileshaydenreal@gmail.com>
2023-02-24 12:34:34 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-3">
+          a
+        </div>
+        <div class="col-9">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root{
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
